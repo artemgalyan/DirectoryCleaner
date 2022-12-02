aadcf778aab7cdcd4463d5a44734e8f65c33c392
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-12-02 20:01:58 +0300

    removed unused logger

--- DirectoryCleanerBackgroundWorker/Worker.cs
+++ DirectoryCleanerBackgroundWorker/Worker.cs
@@ -4,13 +4,6 @@ namespace DirectoryCleanerBackgroundWorker;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
